5611634ce6089fab6ddf198c2a5d45e05c0b7dce
unknown <andrewbergie10@gmail.com>
2025-05-08 13:20:06 -0500

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Register from './pages/register';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
-      {/* Add other routes here */}
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>Home of resume-parser, 
+            <nav>
+                <Link to="/">
+                    <button>Go to Home</button>
+                </Link>
+                or
+                <Link to="/register">
+                    <button>Sign up</button>
+                </Link>
+            </nav>
+        </div>
+    );
+}
+
+export default Home;
